3d917fe8fe4230ff1a124d144b62bc68b0579983
tobecci <ojinakatochukwu@gmail.com>
2019-07-19 16:02:14 +0100

    reconfigureg for easy installation

---
+++ README.md
@@ -0,0 +1,8 @@
+# Medbank
+medbank is a solution to the inefficiencies and errors in data sharing between health care centers
+
+## installation  procedure
+- clone the repository
+- create a database called 'medbank' in your database using phpmyadmin or other equivalent software
+- import the 'medbank.sql' file into your database, after doing this, there should be 'hospitals' and 'patients' tables in your database
+- enter 'application/config/database.php' file on line 78, 80, and 81 change the details as appropriate i.e username database and the database name
---
+++ medbank.sql
@@ -0,0 +1,110 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 19, 2019 at 04:50 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `medbank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hospitals`
+--
+
+CREATE TABLE `hospitals` (
+  `id` int(11) NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `name` text NOT NULL,
+  `type` text NOT NULL,
+  `location` text NOT NULL,
+  `hospital_id` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `hospitals`
+--
+
+INSERT INTO `hospitals` (`id`, `username`, `password`, `name`, `type`, `location`, `hospital_id`) VALUES
+(1, 'sphinx', '$2y$10$VWVwqdEhGfF0YwiL6LThveP6/w2nWPGw2AFjsHj6ooGorc8T5s54O', 'sphinx care', 'optometric', 'lagos state', 'hospital5d31d83bd465c7.14918859');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patients`
+--
+
+CREATE TABLE `patients` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `dob` text NOT NULL,
+  `phone` text NOT NULL,
+  `email` text NOT NULL,
+  `address` text NOT NULL,
+  `gender` text NOT NULL,
+  `ethnicity` text NOT NULL,
+  `language` text NOT NULL,
+  `genotype` text NOT NULL,
+  `blood_group` text NOT NULL,
+  `height` text NOT NULL,
+  `nationality` text NOT NULL,
+  `religion` text NOT NULL,
+  `registered_hospital` text NOT NULL,
+  `medical_history` text NOT NULL,
+  `next_of_kin` text NOT NULL,
+  `patient_id` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `hospitals`
+--
+ALTER TABLE `hospitals`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `patients`
+--
+ALTER TABLE `patients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `hospitals`
+--
+ALTER TABLE `hospitals`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `patients`
+--
+ALTER TABLE `patients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
